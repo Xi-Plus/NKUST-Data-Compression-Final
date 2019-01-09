bd3aafc2b97308c13945b11aeab9a986bea6de73
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-10 00:37:26 +0800

    Add error

--- compress.py
+++ compress.py
@@ -11,13 +11,13 @@ algolist = {
 	"xp03": [[XIPLUS03_encode, [-2, 2, 32]], [XIPLUS03_decode, []]],
 }
 
-
 ctype = sys.argv[1]
-algo = sys.argv[2].lower()
-finname = sys.argv[3]
-foutname = sys.argv[4]
 
 if ctype == "encode":
+	algo = sys.argv[2].lower()
+	finname = sys.argv[3]
+	foutname = sys.argv[4]
+
 	if algo not in algolist:
 		exit("alog not found")
 	print("encode by {}".format(algo))
@@ -36,6 +36,10 @@ if ctype == "encode":
 	print("compression ratio: {}".format(oldsize/newsize))
 
 elif ctype == "decode":
+	algo = sys.argv[2].lower()
+	finname = sys.argv[3]
+	foutname = sys.argv[4]
+
 	if algo not in algolist:
 		exit("alog not found")
 	print("decode by {}".format(algo))
@@ -51,5 +55,16 @@ elif ctype == "decode":
 	print("spend {} s".format(time.time()-start))
 	newsize = os.path.getsize(foutname)
 	print("new size: {}".format(newsize))
+
+elif ctype == "error":
+	fname1 = sys.argv[2]
+	fname2 = sys.argv[3]
+
+	img1 = cv2.imread(fname1, cv2.IMREAD_GRAYSCALE)
+	img2 = cv2.imread(fname2, cv2.IMREAD_GRAYSCALE)
+
+	diff = img2 - img1
+	print("error", math.sqrt(np.average(np.square(diff.flatten()))))
+
 else:
 	print("nothing to do")
